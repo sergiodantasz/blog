69e88886fa8c6a6adce7fa5880e9f11dfef518cc
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-15 23:28:43 -0300

    feat(repositories): add waiting simulation to post repository

--- src/repositories/post-repository.ts
+++ src/repositories/post-repository.ts
@@ -11,6 +11,8 @@ export interface PostRepository {
 const ROOT_DIR = process.cwd();
 const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
 
+const WAITING_TIME_IN_MS = 5000;
+
 export class JsonPostRepository implements PostRepository {
   private async loadPostsFromJsonFile(): Promise<Post[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, { encoding: 'utf-8' });
@@ -18,11 +20,20 @@ export class JsonPostRepository implements PostRepository {
     return parsedJson.posts;
   }
 
+  private async simulateWaiting() {
+    if (WAITING_TIME_IN_MS <= 0) return;
+    await new Promise((resolve) => {
+      setTimeout(resolve, WAITING_TIME_IN_MS);
+    });
+  }
+
   async findAll(): Promise<Post[]> {
+    await this.simulateWaiting();
     return this.loadPostsFromJsonFile();
   }
 
   async findById(id: Post['id']) {
+    await this.simulateWaiting();
     const posts = await this.findAll();
     const post = posts.find((p) => p.id === id);
     if (!post) {
