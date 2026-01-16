7fc2c1d66cf6306d30c83c8c76085fede00bafb1
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-16 15:51:28 -0300

    feat(ui): add Header component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 
+import { Header } from '@/components/ui/header';
 import { PostList } from '@/components/ui/post-list';
 import { Spinner } from '@/components/ui/spinner';
 
 export default async function Page() {
   return (
     <div>
-      <h1>Home</h1>
+      <Header />
       <Suspense fallback={<Spinner />}>
         <PostList />
       </Suspense>
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1 className='pb-8 text-4xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl'>Blog</h1>
+    </header>
+  );
+}
