7b3f023a5a0658273cb782b34de7144830c9e3d4
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-16 23:11:29 -0300

    feat(posts): add functions to sort and group posts by date

---
+++ src/lib/posts.ts
@@ -0,0 +1,29 @@
+import { Post } from '@/models/post';
+
+export function sortPostsByDate(posts: Post[], direction: 'asc' | 'desc') {
+  return [...posts].sort((a, b) => {
+    const diff = b.createdAt.getTime() - a.createdAt.getTime();
+    return direction === 'asc' ? diff : -diff;
+  });
+}
+
+export function groupPostsByYear(posts: Post[]) {
+  return posts.reduce<Record<string, Post[]>>((groupedPosts, post) => {
+    const postYear = new Date(post.createdAt).getFullYear().toString();
+    groupedPosts[postYear] ??= [];
+    groupedPosts[postYear].push(post);
+    return groupedPosts;
+  }, {});
+}
+
+export function sortGroupedPostsByYear(groupedPosts: Record<string, Post[]>) {
+  return Object.entries(groupedPosts)
+    .sort((a, b) => {
+      const diff = b[0].localeCompare(a[0]);
+      return diff;
+    })
+    .map(([year, posts]) => ({
+      year,
+      posts: sortPostsByDate(posts, 'asc'),
+    }));
+}
