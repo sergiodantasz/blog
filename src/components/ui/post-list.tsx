4eaa21da970d35c05d9969e7b7b85831c9d19633
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-16 23:11:48 -0300

    feat(posts): implement grouping and sorting of posts by year in PostList component

--- src/components/ui/post-list.tsx
+++ src/components/ui/post-list.tsx
@@ -1,11 +1,24 @@
 import { postRepository } from '@/repositories/post-repository';
 
+import { groupPostsByYear, sortGroupedPostsByYear } from '@/lib/posts';
+
 export async function PostList() {
   const posts = await postRepository.findAll();
+  const groupedPosts = groupPostsByYear(posts);
+  const sortedPosts = sortGroupedPostsByYear(groupedPosts);
   return (
     <div>
-      {posts.map((post) => (
-        <p key={post.id}>{post.title}</p>
+      {sortedPosts.map(({ year, posts }) => (
+        <div key={year}>
+          <div>{year}</div>
+          <div>
+            {posts.map((post) => (
+              <div key={post.id}>
+                <div>{post.title}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
