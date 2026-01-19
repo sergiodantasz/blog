062d3c6aa398abcb59b4827191fb4ecff3605562
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-18 23:49:48 -0300

    refactor(page): remove ThemeToggle component from the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import { Suspense } from 'react';
 import { Header } from '@/components/ui/header';
 import { PostList } from '@/components/ui/post-list';
 import { Spinner } from '@/components/ui/spinner';
-import { ThemeToggle } from '@/components/ui/theme-toggle';
 
 export default async function Page() {
   return (
     <div>
-      <ThemeToggle />
       <Header />
       <Suspense fallback={<Spinner />}>
         <PostList />
