6f7f6595acc07883ad5050dfa53e69620491dac5
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-19 14:10:24 -0300

    feat(post): enhance post page to generate dynamic metadata and display post title

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,24 @@
+import { Metadata } from 'next';
+
+import { findPostBySlugCached } from '@/lib/queries';
+
+import { Post } from '@/models/post';
+
 type PageProps = {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ slug: Post['slug'] }>;
 };
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
 
 export default async function Page({ params }: PageProps) {
   const { slug } = await params;
-  return <h1>/post/{slug}</h1>;
+  const post = await findPostBySlugCached(slug);
+  return <h1>{post.title}</h1>;
 }
