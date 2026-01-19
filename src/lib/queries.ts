235ad401a21f908af6b1845f75fe5469d6f12e5a
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-19 14:10:02 -0300

    feat(queries): implement caching for post retrieval functions

---
+++ src/lib/queries.ts
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation';
+
+import { cache } from 'react';
+
+import { Post } from '@/models/post';
+
+import { postRepository } from '@/repositories/post-repository';
+
+export const findAllPublishedPostsCached = cache(
+  async () => await postRepository.findAllPublished(),
+);
+
+export const findPostByIdCached = cache(
+  async (id: Post['id']) => await postRepository.findById(id),
+);
+
+export const findPostBySlugCached = cache(async (slug: Post['slug']) => {
+  const post = await postRepository.findBySlug(slug).catch(() => null);
+  if (!post) notFound();
+  return post;
+});
