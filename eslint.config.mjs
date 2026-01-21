adaa314f42cf534f366bad2d36439ff017312ee9
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-21 16:50:07 -0300

    chore(eslint): integrate eslint-plugin-prettier for improved code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,22 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
-import prettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
-  prettier,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+    },
+  },
 ]);
 
 export default eslintConfig;
