8d09a19b52676573ad56d7976ba6ea166ab22acf
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-21 15:31:27 -0300

    feat(error): add ErrorBase component for standardized error display with status code and description

---
+++ src/components/ui/error-base.tsx
@@ -0,0 +1,15 @@
+type ErrorBaseProps = {
+  statusCode: 404 | 500;
+  description: string;
+};
+
+export default function ErrorBase({ statusCode, description }: ErrorBaseProps) {
+  return (
+    <div className='m-auto h-fit border border-dashed border-stone-300 text-center dark:border-stone-700'>
+      <div className='border-b border-dashed border-b-stone-300 p-4 text-4xl font-bold dark:border-b-stone-700'>
+        {statusCode}
+      </div>
+      <p className='p-4'>{description}</p>
+    </div>
+  );
+}
