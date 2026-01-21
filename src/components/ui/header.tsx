ae9153a738a90a7109fb9c2bd4fdf38a2cffaf34
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-21 15:13:10 -0300

    style(header): update blog title styling and add descriptive subtitle for enhanced clarity

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,10 @@
 export function Header() {
   return (
     <header>
-      <h1 className='pb-8 text-4xl font-extrabold sm:text-5xl md:text-6xl lg:text-7xl'>Blog</h1>
+      <h1 className='mb-1 text-3xl font-bold sm:mb-2 sm:text-5xl'>Blog</h1>
+      <p className='text-stone-600 dark:text-stone-400'>
+        Here I try to be a little more technical.
+      </p>
     </header>
   );
 }
