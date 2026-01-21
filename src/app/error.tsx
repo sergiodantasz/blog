f07bfa22404c4bf6c31449afbfbe3f77f9fd4366
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-21 16:24:28 -0300

    feat(error): implement Error component to handle internal server errors with dynamic title update

---
+++ src/app/error.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import ErrorBase from '@/components/ui/error-base';
+
+import { formatTitle } from '@/utils/metadata';
+
+export default function Error() {
+  useEffect(() => {
+    document.title = formatTitle('Internal server error');
+  }, []);
+  return (
+    <ErrorBase
+      statusCode={500}
+      description='A server-side error occurred.'
+    />
+  );
+}
