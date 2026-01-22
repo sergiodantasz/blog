21d52508d19213942fb904b53d166ea11aa191a2
Sérgio Dantas <sergiodnts828@gmail.com>
2026-01-22 12:45:17 -0300

    chore(db): generate drizzle migration

---
+++ src/db/migrations/0000_sticky_snowbird.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`title` text NOT NULL,
+	`slug` text NOT NULL,
+	`excerpt` text NOT NULL,
+	`content` text NOT NULL,
+	`is_published` integer NOT NULL,
+	`created_at` text NOT NULL,
+	`updated_at` text NOT NULL,
+	`author` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_slug_unique` ON `users` (`slug`);
