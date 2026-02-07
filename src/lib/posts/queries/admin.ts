5882e61a91dfdd7cb5aa3b434152518d4bfc031f
Sérgio Dantas <sergiodnts828@gmail.com>
2026-02-06 21:57:08 -0300

    refactor(queries): remove unstable caching from admin functions

--- src/lib/posts/queries/admin.ts
+++ src/lib/posts/queries/admin.ts
@@ -1,4 +1,3 @@
-import { unstable_cache } from 'next/cache';
 import { notFound } from 'next/navigation';
 
 import { reviveDates } from '@/lib/posts/transform';
@@ -8,23 +7,12 @@ import { Post } from '@/models/post';
 import { postRepository } from '@/repositories/post';
 
 export async function findAllCached() {
-  const posts = await unstable_cache(async () => await postRepository.findAll(), ['posts'], {
-    tags: ['posts'],
-  })();
+  const posts = await postRepository.findAll();
   return posts.map(reviveDates);
 }
 
 export async function findByIdCached(id: Post['id']) {
-  const post = await unstable_cache(
-    async (id: Post['id']) => {
-      const post = await postRepository.findById(id).catch(() => null);
-      if (!post) notFound();
-      return post;
-    },
-    ['posts'],
-    {
-      tags: ['posts', `posts:${id}`],
-    },
-  )(id);
+  const post = await postRepository.findById(id).catch(() => null);
+  if (!post) notFound();
   return reviveDates(post);
 }
