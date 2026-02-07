22821defff1132d42e362038a443ee6513d60ca2
Sérgio Dantas <sergiodnts828@gmail.com>
2026-02-06 22:12:39 -0300

    feat(admin): add Header component for admin section with title and description

---
+++ src/components/ui/admin/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header>
+      <h1 className='mb-1 text-3xl font-bold sm:mb-2 sm:text-5xl'>
+        <span className='font-normal text-stone-600 dark:text-stone-400'>Admin ·</span> Blog
+      </h1>
+      <p className='text-stone-600 dark:text-stone-400'>
+        Let&apos;s change everything (or maybe nothing).
+      </p>
+    </header>
+  );
+}
