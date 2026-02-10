cb78096616e7a97f4121df5d85f396e51d1742eb
Sérgio Dantas <sergiodnts828@gmail.com>
2026-02-10 16:43:01 -0300

    feat(schemas): create PostSchema for post validation with title, content, excerpt, and publication status

---
+++ src/schemas/post.ts
@@ -0,0 +1,38 @@
+import sanitizeHtml from 'sanitize-html';
+import z from 'zod';
+
+export const PostSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(3, 'The title must be at least 3 characters long.')
+    .max(120, 'The title must have a maximum of 120 characters.'),
+  content: z
+    .string()
+    .trim()
+    .min(3, 'The content must be at least 3 characters long.')
+    .transform((val) => sanitizeHtml(val)),
+  // author: z
+  //   .string()
+  //   .trim()
+  //   .min(2, 'The author must be at least 2 characters long.')
+  //   .max(30, 'The author must have a maximum of 30 characters.'),
+  excerpt: z
+    .string()
+    .trim()
+    .min(3, 'The excerpt must be at least 3 characters long.')
+    .max(200, 'The excerpt must have a maximum of 200 characters.'),
+  isPublished: z
+    .union([
+      z.literal('on'),
+      z.literal('off'),
+      z.literal('true'),
+      z.literal('false'),
+      z.literal(true),
+      z.literal(false),
+      z.literal(null),
+      z.literal(undefined),
+    ])
+    .default(false)
+    .transform((val) => val === 'on' || val === 'true' || val === true),
+});
