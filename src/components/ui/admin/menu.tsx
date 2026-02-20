9341289d1f1a6f1ddb5001b87d336ab8e40b589b
Sérgio Dantas <sergiodnts828@gmail.com>
2026-02-20 13:13:49 -0300

    feat(menu): add logout functionality with loading state to admin menu

--- src/components/ui/admin/menu.tsx
+++ src/components/ui/admin/menu.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 import Link from 'next/link';
 
-import { HouseIcon, NewspaperIcon, PlusIcon } from 'lucide-react';
+import { useTransition } from 'react';
+
+import { HouseIcon, LogOutIcon, NewspaperIcon, PlusIcon } from 'lucide-react';
+
+import { logout } from '@/actions/auth';
 
 export function Menu() {
+  const [isPending, startTransition] = useTransition();
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    startTransition(async () => {
+      await logout();
+    });
+  };
   return (
     <nav className='mx-auto mb-8 flex w-fit items-center justify-center divide-x divide-dashed divide-stone-300 rounded-lg border border-dashed border-stone-300 text-center text-sm font-medium text-stone-700 sm:mb-16 dark:divide-stone-700 dark:border-stone-700 dark:text-stone-300'>
       <a
@@ -27,7 +40,7 @@ export function Menu() {
         <span>Posts</span>
       </Link>
       <Link
-        className='flex items-center justify-center gap-1 self-stretch rounded-se-[7px] rounded-ee-[7px] px-4 py-2 transition-all hover:bg-stone-300 dark:hover:bg-stone-900'
+        className='flex items-center justify-center gap-1 self-stretch px-4 py-2 transition-all hover:bg-stone-300 dark:hover:bg-stone-900'
         href='/admin/posts/new'
       >
         <PlusIcon
@@ -36,6 +49,17 @@ export function Menu() {
         />
         <span>New post</span>
       </Link>
+      <button
+        className='flex cursor-pointer items-center justify-center gap-1 self-stretch rounded-se-[7px] rounded-ee-[7px] px-4 py-2 transition-all hover:bg-stone-300 disabled:cursor-wait disabled:opacity-50 dark:hover:bg-stone-900'
+        onClick={handleLogout}
+        disabled={isPending}
+      >
+        <LogOutIcon
+          size={16}
+          className='shrink-0'
+        />
+        <span>Log out</span>
+      </button>
     </nav>
   );
 }
